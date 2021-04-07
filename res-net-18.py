ccd0fc8cbeb264dafdc44550f445bbe715117999
tomas-nekut <58033664+tomas-nekut@users.noreply.github.com>
2021-04-07 08:49:56 +0200

    cleanup

--- res-net-18.py
+++ res-net-18.py
@@ -11,17 +11,18 @@ from sklearn.manifold import TSNE
 import pandas as pd
 from PIL import Image
 from google.colab import drive
-# drive.mount('/content/drive/')
-# !unzip /content/drive/MyDrive/KNN/lfw-deepfunneled.zip
 
-batch_size = 64
+batch_size = 8
 epoch_num = 1000
-num_classes = 1700
-lr = 0.1
+model_path = "model.pt"
+num_classes = 1700 #casia 10575  
+lr = 0.001
+wd = 0.0001
 target_test_acc = 80
-visualization_cnt = 5
-visualization_colors = ["green", "blue", "red", "black", "yellow"]
+visualization_cnt = 10
+visualization_colors = ["b", "g", "r", "c", "m", "y", "k", "w", "orange", "gray"]
 
+<<<<<<< Updated upstream
 transform=Compose([
     Resize(224), #28x28 -> 224x224
     ToTensor()#,
@@ -43,6 +44,14 @@ test_data = datasets.MNIST(
     transform=transform
 )
 '''
+=======
+#drive.mount('/content/drive/')
+#ODKOMENTOVAT JEDNO PRI PRVOM SPUSTENI
+#CASIA dataset ako zip, musi byt uploadnuty vo vasom google drive v zlozke KNN
+#!unzip /content/drive/MyDrive/KNN/CASIA.zip
+#LFW dataset ako zip, musi byt uploadnuty vo vasom google drive v zlozke KNN
+#!unzip /content/drive/MyDrive/KNN/lfw-deepfunneled.zip
+>>>>>>> Stashed changes
 
 class LFWDataset(Dataset):
   def __init__(self, csv_file, root_dir, transform=None):
@@ -59,29 +68,39 @@ class LFWDataset(Dataset):
     y_label = torch.tensor(int(self.annotations.iloc[index,1]))
 
     if self.transform:
+<<<<<<< Updated upstream
       image = image.convert("RGB") 
+=======
+      image = image.convert("RGB")
+>>>>>>> Stashed changes
       image = self.transform(image)
     
     return (image, y_label)
 
-dataset = LFWDataset(csv_file = 'lfw-deepfunneled/lfw.csv', root_dir = 'lfw-deepfunneled', transform = transform)
+transform=Compose([Resize(224), ToTensor()]) 
+
+# ODKOMENTOVAT JEDNO
+# LFW dataset
+dataset = LFWDataset(csv_file = 'lfw-deepfunneled/lfw-deepfunneled/lfw.csv', root_dir = 'lfw-deepfunneled/lfw-deepfunneled', transform = transform) 
+# CASIA dataset
+#dataset = LFWDataset(csv_file = 'CASIA/casia2.csv', root_dir = 'CASIA', transform = transform)
 
 train_size = int(len(dataset)*0.8)
 train_set, test_set = torch.utils.data.random_split(dataset, [train_size, len(dataset) - train_size])
-
 train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-test_dataloader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
-
-'''
-# Create data loaders
-train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
-test_dataloader = DataLoader(test_data, batch_size=100)
-'''
+test_dataloader = DataLoader(test_set, batch_size=1000, shuffle=True)
 
 # Vizualization data
-X, Y = next(iter(test_dataloader))
-mask = np.vectorize(lambda x: x < visualization_cnt)(Y)
-visualization_data = X[mask],Y[mask]
+first = True
+for X, Y in train_dataloader:
+  mask = np.vectorize(lambda x: x < visualization_cnt)(Y)
+  if first:
+    visualization_data_X,visualization_data_Y = X[mask],Y[mask]
+    first = False
+  else:
+    visualization_data_X = np.vstack((visualization_data_X,X[mask]))
+    visualization_data_Y = np.hstack((visualization_data_Y,Y[mask]))
+visualization_data = torch.from_numpy(visualization_data_X), torch.from_numpy(visualization_data_Y)
 
 # Get cpu or gpu device for training.
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -92,7 +111,7 @@ class InputStemBlock(nn.Module):
     def __init__(self, channels_in: int, channels_out: int):
         super(InputStemBlock, self).__init__()
         self.operation = nn.Sequential(
-            nn.Conv2d(channels_in, channels_out, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.Conv2d(channels_in, channels_out, kernel_size=7, stride=2, padding=3, bias=True),
             nn.BatchNorm2d(channels_out),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
@@ -105,10 +124,11 @@ class ResidualBlock(nn.Module):
     def __init__(self, channels_in: int, channels_out: int, stride: int):
         super(ResidualBlock, self).__init__()
         self.operation = nn.Sequential(
-            nn.Conv2d(channels_in, channels_out, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.Conv2d(channels_in, channels_out, kernel_size=3, stride=stride, padding=1, bias=True),
             nn.BatchNorm2d(channels_out),
             nn.ReLU(),
-            nn.Conv2d(channels_out, channels_out, kernel_size=3, stride=1, padding=1, bias=False),
+            #nn.Conv2d(channels_out, channels_out, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(channels_out, channels_out, kernel_size=1, stride=1, padding=0, bias=True),
             nn.BatchNorm2d(channels_out)
         )
         #results of self.operation and self.identity have to have same C,W,H to perform addition
@@ -118,76 +138,13 @@ class ResidualBlock(nn.Module):
         else:
             #correct C,W,H by 1x1 conv
             self.identity = nn.Sequential(
-                nn.Conv2d(channels_in, channels_out, kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(channels_in, channels_out, kernel_size=1, stride=stride, bias=True),
                 nn.BatchNorm2d(channels_out)
             )        
     def forward(self, x):
         output = self.operation(x) + self.identity(x)
         output = f.relu(output)
         return output
-
-'''
-#Conv block, resnet50
-class ConvBlock(nn.Module):
-    def __init__(self, batch_size: int, channels_in: int, channels_out: int):
-        super(NeuralNetwork, self).__init__()
-        self.conv1x1a = nn.Conv2d(self.channels_in, self.channels_out, kernel_size=1, stride=1, padding=0, bias=False)
-        self.conv1x1b = nn.Conv2d(self.channels_in, self.channels_out, kernel_size=1, stride=1, padding=0, bias=False)
-        self.conv1x1c = nn.Conv2d(self.channels_in, self.channels_out, kernel_size=1, stride=1, padding=0, bias=False)
-        #Do batch_norm patří batch_size, nebo channels_out?
-        self.batch_norm = nn.BatchNorm2d(batch_size)
-        self.conv3x3 = nn.Conv2d(self.channels_in, self.channels_out, kernel_size=3, stride=2, padding=1, bias=False)
-        self.relu = nn.ReLU()
-
-    def forward (self, input: Tensor) -> Tensor:
-        output = self.conv1x1a(input)
-        output = self.batch_norm(output)
-        output = self.relu(output)
-
-        forwarded_output = self.conv1x1b(input)
-        forwarded_output = self.batch_norm(forwarded_output)
-
-        output = self.conv3x3(output)
-        output = self.batch_norm(output)
-        output = self.relu(output)
-
-        output = self.conv1x1c(output)
-        output = self.batch_norm(output)
-
-        output = torch.add(output, forwarded_output)
-        output = self.relu(output)
-
-        return (output)
-
-#Identity block, resnet50
-class IdentityBlock(nn.Module):
-    def __init__(self, batch_size: int, channels_in: int, channels_out: int):
-        super(NeuralNetwork, self).__init__()
-        self.conv1x1a = nn.Conv2d(self.channels_in, self.channels_out, kernel_size=1, stride=1, padding=0, bias=False)
-        self.conv1x1b = nn.Conv2d(self.channels_in, self.channels_out, kernel_size=1, stride=1, padding=0, bias=False)
-        self.conv1x1c = nn.Conv2d(self.channels_in, self.channels_out, kernel_size=1, stride=1, padding=0, bias=False)
-        #Do batch_norm patří batch_size, nebo channels_out?
-        self.batch_norm = nn.BatchNorm2d(batch_size)
-        self.conv3x3 = nn.Conv2d(self.channels_in, self.channels_out, kernel_size=3, stride=1, padding=1, bias=False)
-        self.relu = nn.ReLU()
-
-    def forward (self, input: Tensor) -> Tensor:
-        output = self.conv1x1a(input)
-        output = self.batch_norm(output)
-        output = self.relu(output)
-
-        output = self.conv3x3(output)
-        output = self.batch_norm(output)
-        output = self.relu(output)
-
-        output = self.conv1x1c(output)
-        output = self.batch_norm(output)
-        
-        output = torch.add(output, input)
-        output = self.relu(output)
-
-        return (output)
-'''
 
 #ResNet18
 class ResNet18(nn.Module):
@@ -228,30 +185,35 @@ class ResNet18(nn.Module):
         return x
 
 def validate(model):
-    model.train(mode=False)
-    correct = total = 0 
-    for inputs, labels in test_dataloader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        outputs = model(inputs)
-        predicted = torch.argmax(outputs.data, dim=1)
-        correct += predicted.eq(labels.data).cpu().sum()
-        total += labels.size(0)
+    model.eval()
+    with torch.no_grad():
+        correct = total = 0 
+        for inputs, labels in test_dataloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            predicted = torch.argmax(outputs.data, dim=1)
+            correct += predicted.eq(labels.data).cpu().sum()
+            total += labels.size(0)
     return (100. * correct / total).item()
 
 def visualize_embeding(model):  
-    inputs,labels = visualization_data
-    model.train(mode=False)
-    outputs = model.encode(inputs)
-    outputs_embedded = TSNE(n_components=2).fit_transform(outputs.detach().numpy())
-    x,y = np.hsplit(outputs_embedded, 2)
-    x,y = x.flatten(), y.flatten()
-    for i in range(len(x)):
-        plt.scatter(x[i], y[i], color=visualization_colors[labels[i]])
-    plt.show()
+    model.eval()
+    with torch.no_grad():
+        inputs,labels = visualization_data
+        inputs,labels = inputs.to(device), labels.to(device)
+        outputs = model.encode(inputs)
+        outputs_embedded = TSNE(n_components=2).fit_transform(outputs.cpu().detach().numpy())
+        x,y = np.hsplit(outputs_embedded, 2)
+        x,y = x.flatten(), y.flatten()
+        for i in range(len(x)):
+            plt.scatter(x[i], y[i], color=visualization_colors[labels[i]])
+        plt.show()
+
+
 
 model = ResNet18(num_classes).to(device)
-criterion = nn.CrossEntropyLoss ()
-optimizer = optim.SGD(model.parameters(), lr=lr) 
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=wd) 
 
 #training
 #foreach epoch
@@ -268,16 +230,21 @@ for e in range(epoch_num):
         optimizer.step()
 
         loss_sum += loss.item()
+        avg_loss = loss_sum / (b+1)
+
         predicted = torch.argmax(outputs.data, dim=1)
         total += labels.size(0)
-        correct += predicted.eq(labels.data).cpu().sum()
-        avg_loss = loss_sum / (b+1)
+        correct += predicted.eq(labels.data).cpu().sum() 
         train_acc = (100. * correct / total).item()
-        test_acc = validate(model)
-        print("epoch:", e, "batch:", b, "avg loss:", "%.3f" % avg_loss, "train acc:", train_acc, "test acc: ", test_acc)
+        
+        if b%3 == 0:
+          print("epoch:", e, "batch:", b, "avg loss:", "%.3f" % avg_loss, "epoch avg train acc:", train_acc)
 
-        if test_acc > target_test_acc:
-            break
+    test_acc = validate(model)
+    print("epoch:", e, "test acc: ", test_acc)
+    if test_acc > target_test_acc:
+        break
 
+torch.save(model.state_dict(), model_path)
 visualize_embeding(model)
 
