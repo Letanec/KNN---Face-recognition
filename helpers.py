f30cdfe4116922de0fafa2001490e42c83395bad
tomas-nekut <58033664+tomas-nekut@users.noreply.github.com>
2021-04-17 17:03:09 +0200

    datasets, train prints

---
+++ helpers.py
@@ -0,0 +1,28 @@
+
+import torch
+import os
+from custom_dataset import CustomDataset
+from log import Log
+
+def get_datasets(batch_size, batch_size_test=1000):
+    transform=Compose([Resize(96), ToTensor()]) 
+    #CASIA DATASET
+    dataset = CustomDataset(csv_file = 'datasets/CASIA/casia_train.csv', root_dir = 'datasets/CASIA', transform = transform) 
+    train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    dataset = CustomDataset(csv_file = 'datasets/CASIA/casia_test.csv', root_dir = 'datasets/CASIA', transform = transform) 
+    test_dataloader = DataLoader(dataset, batch_size=batch_size_test)
+    dataset = CustomDataset(csv_file = 'datasets/CASIA/casia_pairs.csv', root_dir = 'datasets/CASIA', transform = transform, pairs=True) 
+    casia_pairs_dataloader = DataLoader(dataset)
+    #LFW DATASET
+    dataset = CustomDataset(csv_file = 'datasets/LFW/lfw_pairs.csv', root_dir = 'datasets/LFW', transform = transform, pairs=True) 
+    lfw_pairs_dataloader = DataLoader(dataset)
+    return (train_dataloader, test_dataloader, casia_pairs_dataloader, lfw_pairs_dataloader)
+
+def create_logs():
+    path = "outputs"
+    logs_names = ["acc","test_acc","ver","lfw_ver","loss","tf","lfw_tf"]
+    logs = []
+    for ln in logs_names:
+        log = Log(os.path.join(path, ln))
+        logs.append(log)
+    return logs
--- knn.py
+++ knn.py
@@ -7,80 +7,54 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, ToPILImage, Lambda, Compose, Resize
 import matplotlib.pyplot as plt
 import numpy as np 
-from sklearn.manifold import TSNE
 import pandas as pd
 from PIL import Image
 from scipy.spatial import distance
-from sklearn.datasets import fetch_lfw_pairs
-import calendar;
-import time;
-import argparse
-
+import calendar
+import time
 from res_net import ResNet18, ResNet50
-from casia_dataset import CasiaDataset
-from validation import validate, print_ROC
+from validation import verify, validate, tars_fars, print_ROC
+from helpers import get_datasets, create_logs
 
-#LFW DATASET
-# fetch_lfw_pairs = fetch_lfw_pairs(subset='10_folds', funneled=False, color=True, resize=96/250, slice_=None) #lepší začít s nižším rozlišením než 224
-# lfw_pairs = fetch_lfw_pairs.pairs
-# lfw_labels = fetch_lfw_pairs.target
-# lfw_pairs = lfw_pairs.transpose((0, 1, 4, 2, 3))
-# lfw_pairs = Tensor(lfw_pairs)
-# print(lfw_pairs.shape)
-
-
-def get_dataset(short, batch_size):
-    transform=Compose([Resize(96), ToTensor()]) 
-    dataset = CasiaDataset(csv_file = '../../CASIA/casia2.csv', root_dir = '../../CASIA', transform = transform) 
-    
-    if short:
-        short_len = 10000
-        dataset_short, _ = torch.utils.data.random_split(dataset, [short_len, len(dataset) - short_len])
-        train_dataloader = DataLoader(dataset_short, batch_size=batch_size, shuffle=True)
-    else:
-        train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    
-    return train_dataloader
-
-def create_output_files():
-    ts = calendar.timegm(time.gmtime())
-    TARS_and_FARs_file = open(f"./outputs/all_TARs_and_FARs_{ts}.out", "w").close()
-    test_vers_file = open(f"./outputs/test_accs{ts}.out", "w").close()
-    train_accs_file = open(f"./outputs/train_accs{ts}.out", "w").close()
-    loss_file = open(f"./outputs/loss{ts}.out", "w").close()
-
-    return ts
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--batch_size", help="set batch size", action="store", type=int, default=32)
-    parser.add_argument("-t", "--train_print", help="set period of printing training progress", action="store", type=int, default=10)
-    parser.add_argument("-v", "--validation_period", help="set period of validation evaluation", action="store", type=int, default=500)
+    parser.add_argument("-t", "--train_print_period", help="set period of printing training progress", action="store", type=int, default=10)
+    parser.add_argument("-v", "--test_period", help="set period of validation", action="store", type=int, default=100)
+    parser.add_argument("-v", "--ver_period", help="set period of verification", action="store", type=int, default=1500)
+    parser.add_argument("-v", "--lfw_ver_period", help="set period of lfw verification", action="store", type=int, default=6000)  
     args = parser.parse_args()
     
     # Constants
     batch_size = args.batch_size
     train_print_period = args.train_print
     test_period = args.validation_period
-    model_path = "./model.pt"
+    ver_period = args.ver_period
+    lfw_ver_period = args.lfw_ver_period
+    model_path = "./model.pt" 
     num_classes = 10575   
     embeding_size = 512    
     accs = losses = test_vers =  []
 
-    # Get cpu or gpu device for training.
+    # Get cpu or gpu device
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Using {} device".format(device))
 
-    train_dataloader = get_dataset(False, batch_size)
+    # Datasets
+    train_dataloader, test_dataloader, casia_pairs_dataloader, lfw_pairs_dataloader = get_datasets(batch_size)
+    
+    # Logs
+    acc_log, test_acc_log, ver_log, lfw_ver_log, loss_log, tf_log = create_logs()
+
+    # Model, criterionm, optimizer
     model = ResNet50(num_classes=num_classes, emb_size=embeding_size).to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001) #rozumná lr je 0.001
+    optimizer = optim.Adam(model.parameters(), lr=0.001) 
 
-    ts = create_output_files()
-
-    #training
+    # Training
     i = 0
-    best_test_ver = 0
+    best_ver = 0
     for e in range(6): 
         model.train()
         loss_sum = correct = total = 0
@@ -98,50 +72,37 @@ def main():
             predicted = torch.argmax(outputs.data, dim=1)
             total += labels.size(0)
             correct += predicted.eq(labels.data).cpu().sum() 
-            train_acc = (100. * correct / total).item()
             
             if i>0 and i % train_print_period == 0:
-                print("iteration:", i, "epoch:", e, "batch:", b, "avg loss:", "%.3f" % avg_loss, "epoch avg train acc:", train_acc)
-                with open(f"./outputs/train_accs{ts}.out", "a") as train_accs_file:
-                    train_accs_file.write(f'{train_acc}\n')
-                with open(f"./outputs/loss{ts}.out", "a") as loss_file:
-                    loss_file.write(f'{avg_loss}\n')
-                accs.append(train_acc)
-                losses.append(avg_loss)
+                train_acc = (correct / total).item()
+                acc_log.print(i,e,train_acc)
+                loss_log.print(i,e,avg_loss)
 
-            # if i>0 and i%test_period == 0:
-            #     test_ver = validate(model, lfw_pairs, lfw_labels, device, ts)
-            #     model.train()
-            #     print("epoch:", e, "test ver: ", test_ver)
-            #     test_vers.append(test_ver)
+            if i>0 and i % test_period == 0:
+                test_acc = validate(model, test_dataloader, device)
+                model.train()
+                test_acc_log.print(i,e,test_acc)
 
-            #     if test_ver > best_test_ver:  
-            #         best_test_ver = test_ver 
-            #         #print_ROC(model, lfw_pairs, lfw_labels, device)         
-            #         torch.save(model.state_dict(), model_path)
+            if i>0 and i % ver_period == 0:
+                ver = verify(model, casia_pairs_dataloader, device)
+                ver_log.print(i,e,ver)
+                tf = tars_fars(model, casia_pairs_dataloader, device)
+                model.train() 
+                tf_log.print(i,e,tf)
+                print_ROC(tf[0], tf[1])
+                if ver > best_ver:  
+                    best_ver = ver     
+                    torch.save(model.state_dict(), model_path)
 
-            # if i == 20000 or i == 28000:
-            #     optimizer.param_groups[0]['lr'] /= 10
-
-            # if i == 32000:
-            #     break
+            if i>0 and i % lfw_ver_period == 0:
+                lfw_ver = verify(model, lfw_pairs_dataloader, device)
+                lfw_tf = tars_fars(model, lfw_pairs_dataloader, device)
+                model.train()
+                lfw_ver_log.print(i,e,lfw_ver)
+                lfw_tf_log.print(i,e,lfw_tf)
 
             i+=1
 
-    TARS_and_FARs_file.close()
-    test_vers_file.close()
-
-
-
-    print("acc")
-    plt.plot(accs)
-    plt.show()
-    print("loss")
-    plt.plot(losses)
-    plt.show()
-    print("test vers")
-    plt.plot(test_vers)
-    plt.show()
 
 if __name__ == "__main__":
     main()
